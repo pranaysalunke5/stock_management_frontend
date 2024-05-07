fb6c35d30b3e613bbb9c47a07226663483bc0ebd
Pranay Salunke <pranaysalunke5@gmail.com>
2024-05-07 12:58:09 +0530

    new

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -120,7 +120,6 @@ const Profile = () => {
                   isMulti
                 />
                 
-                {/* Quantity input */}
                 <input
                   type="number"
                   value={quantity}
@@ -158,87 +157,6 @@ const Profile = () => {
           </div>
         </div>
       )}
-              {/* <tr class="bg-white border-b  ">
-                <td class="w-4 p-4">
-                  <div class="flex items-center">
-                    <input
-                      id="checkbox-table-search-1"
-                      type="checkbox"
-                      class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label for="checkbox-table-search-1" class="sr-only">
-                      checkbox
-                    </label>
-                  </div>
-                </td>
-                <th
-                  scope="row"
-                  class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-                >
-                  Apple MacBook Pro 17 "
-                </th>
-                <td class="px-6 py-4 text-gray-900 ">Silver</td>
-                <td class="px-6 py-4 text-gray-900 ">Laptop</td>
-                <td class="px-6 py-4 text-gray-900 ">Yes</td>
-                <td class="px-6 py-4 text-gray-900 ">Yes</td>
-                <td class="px-6 py-4 text-gray-900 ">$2999</td>
-                <td class="px-6 py-4 text-gray-900 ">3.0 lb.</td>
-                <td class="flex items-center px-6 py-4">
-                  <a
-                    href="#"
-                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                  >
-                    Edit
-                  </a>
-                  <a
-                    href="#"
-                    class="font-medium text-red-600 dark:text-red-500 hover:underline ms-3"
-                  >
-                    Remove
-                  </a>
-                </td>
-              </tr> */}
-              {/* <tr class="bg-white border-b  ">
-                <td class="w-4 p-4">
-                  <div class="flex items-center">
-                    <input
-                      id="checkbox-table-search-1"
-                      type="checkbox"
-                      class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                    />
-                    <label for="checkbox-table-search-1" class="sr-only">
-                      checkbox
-                    </label>
-                  </div>
-                </td>
-                <th
-                  scope="row"
-                  class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
-                >
-                  Apple MacBook Pro 17 "
-                </th>
-                <td class="px-6 py-4 text-gray-900 ">Silver</td>
-                <td class="px-6 py-4 text-gray-900 ">Laptop</td>
-                <td class="px-6 py-4 text-gray-900 ">Yes</td>
-                <td class="px-6 py-4 text-gray-900 ">Yes</td>
-                <td class="px-6 py-4 text-gray-900 ">$2999</td>
-                <td class="px-6 py-4 text-gray-900 ">3.0 lb.</td>
-                <td class="flex items-center px-6 py-4">
-                  <a
-                    href="#"
-                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
-                  >
-                    Edit
-                  </a>
-                  <a
-                    href="#"
-                    class="font-medium text-red-600 dark:text-red-500 hover:underline ms-3"
-                  >
-                    Remove
-                  </a>
-                </td>
-              </tr> */}
-          
             </tbody>
           </table>
         </div>
